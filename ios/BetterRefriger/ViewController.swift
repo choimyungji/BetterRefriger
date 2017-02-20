663fc1b75b0cc1def3318d639e16781323433db3
maengji <maeng@MaengMBPR.local>
2017-02-20 23:00:28 +0900

    add a new project

---
+++ ios/BetterRefriger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BetterRefriger
+//
+//  Created by Myungji on 2017. 2. 12..
+//  Copyright © 2017년 maengji.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
