a0a840d1b7be2f47a7fe74a128dce8f695710982
Myungji Choi <choimyungji@gmail.com>
2018-11-12 23:21:33 +0900

    delete unused class

--- ios/BetterRefriger/ViewController.swift
+++ ios/BetterRefriger/ViewController.swift
@@ -142,13 +142,3 @@ class ViewController: UITableViewController, FoodInputViewControllerDelegate {
     return cell!
   }
 }
-
-class SubtitleTableViewCell: UITableViewCell {
-  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-    super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
-  }
-
-  required init?(coder aDecoder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-}
