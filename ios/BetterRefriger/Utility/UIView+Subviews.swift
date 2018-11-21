be873a9458fb23acae2ff003fead35494ab4bc36
Myungji Choi <choimyungji@gmail.com>
2018-11-21 09:43:30 +0900

    relocate utilities and extensions

---
+++ ios/BetterRefriger/Utility/UIView+Subviews.swift
@@ -0,0 +1,17 @@
+//
+//  UIView+Subviews.swift
+//  BetterRefriger
+//
+//  Created by 최명지 on 21/11/2018.
+//  Copyright © 2018 maengji.com. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+  func addSubviews(_ views: [UIView]) {
+    for view in views {
+      addSubview(view)
+    }
+  }
+}
