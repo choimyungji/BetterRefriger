63f6c8f923b271b3ecef395e48acb0f06dd5f5f8
Myungji Choi <choimyungji@gmail.com>
2019-03-01 23:33:05 +0900

    divide notification content

--- ios/BetterRefriger/Service/NotificationManager.swift
+++ ios/BetterRefriger/Service/NotificationManager.swift
@@ -15,30 +15,37 @@ class NotificationManager {
   var name: String?
   var expireDate: Date?
 
+  func makeNotificationContent() -> UNMutableNotificationContent? {
+    guard let name = self.name else { return nil }
+
+    let content = UNMutableNotificationContent()
+    content.categoryIdentifier = self.requestId
+    content.body = "\(name)의 유통기한이 다 되어갑니다."
+
+    return content
+  }
+
   func requestNotification() {
-    guard let name = self.name, let expireDate = self.expireDate else { return }
+    guard let expireDate = self.expireDate else { return }
 
     let center = UNUserNotificationCenter.current()
     let options: UNAuthorizationOptions = [.alert, .sound]
 
     center.requestAuthorization(options: options) { (granted, error) in
-      if granted {
-        let content = UNMutableNotificationContent()
-        content.categoryIdentifier = self.requestId
-        content.body = "\(name)의 유통기한이 다 되어갑니다."
-        let calendar = Calendar.current
+      guard granted else { return }
+      guard let content = self.makeNotificationContent() else { return }
 
-        var components = calendar.dateComponents([.hour, .minute, .second], from: expireDate)
+      let calendar = Calendar.current
+      var components = calendar.dateComponents([.hour, .minute, .second], from: expireDate)
 
-        components.hour = 21
-        components.minute = 12
+      components.hour = 21
+      components.minute = 12
 
-        let trigger = UNCalendarNotificationTrigger(dateMatching: components, repeats: true)
-        let request = UNNotificationRequest(identifier: self.requestId, content: content, trigger: trigger)
-        let center = UNUserNotificationCenter.current()
-        center.add(request) { (error) in
-          print(error?.localizedDescription ?? "")
-        }
+      let trigger = UNCalendarNotificationTrigger(dateMatching: components, repeats: true)
+      let request = UNNotificationRequest(identifier: self.requestId, content: content, trigger: trigger)
+      let center = UNUserNotificationCenter.current()
+      center.add(request) { (error) in
+        print(error?.localizedDescription ?? "")
       }
     }
   }
