45770bdd4c91c6e36c89373b35b15bf59323c5ee
Myungji Choi <choimyungji@gmail.com>
2019-04-06 09:22:22 +0900

    add test codes

--- ios/BetterRefrigerTests/BetterRefrigerTests.swift
+++ ios/BetterRefrigerTests/BetterRefrigerTests.swift
@@ -10,24 +10,19 @@ import XCTest
 
 class BetterRefrigerTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+  override func setUp() {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+  }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+  func test_식품을_등록하면_푸시메시지를_새로_생성한다() {
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+  }
 
+  func test_식품을_삭제하면_푸시메시지를_새로_생성한다() {
+
+  }
 }
