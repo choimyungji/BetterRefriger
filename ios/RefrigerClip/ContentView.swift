ddfa67e5fd9d50a8b79a0f8c882d6e3931c2fe2e
Myungji Choi <choimyungji@gmail.com>
2021-07-31 22:59:48 +0900

    add app clip

---
+++ ios/RefrigerClip/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  RefrigerClip
+//
+//  Created by Myungji Choi on 2021/07/30.
+//  Copyright © 2021 maengji.com. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios/RefrigerClip/RefrigerClipApp.swift
@@ -0,0 +1,18 @@
+//
+//  RefrigerClipApp.swift
+//  RefrigerClip
+//
+//  Created by Myungji Choi on 2021/07/30.
+//  Copyright © 2021 maengji.com. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct RefrigerClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
