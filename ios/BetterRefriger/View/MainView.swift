b22589b0100259f12ba2f486d606f67da9a75404
Myungji Choi <choimyungji@gmail.com>
2021-07-31 23:01:56 +0900

    add navigation and model

--- ios/BetterRefriger/View/MainView.swift
+++ ios/BetterRefriger/View/MainView.swift
@@ -9,31 +9,50 @@ import SwiftUI
 import CoreData
 
 struct MainView: View {
-    var foods: [NSManagedObject]
+    var viewModel: MainViewModel!
+    var foods: [FoodModel] {
+        viewModel.foods(spaceType: SpaceType())
+    }
 
     var body: some View {
-        ZStack {
-            List {
-                Text("Hello, world!")
-                    .padding()
-            }
-            HStack {
-                VStack {
-                    Spacer()
-                    SelectAreaButtonSU()
-                        .frame(width: 54, height: 54)
-                    SelectAreaButtonSU()
-                        .frame(width: 54, height: 54)
+        NavigationView {
+            ZStack {
+                List {
+                    ForEach(foods) { food in
+                        Text(food.name)
+                    }
                 }
-                Spacer()
+                HStack {
+                    VStack {
+                        Spacer()
+                        SelectAreaButtonSU(title: "냉장")
+                            .frame(width: 54, height: 54)
+                        SelectAreaButtonSU(title: "냉동")
+                            .frame(width: 54, height: 54)
+                    }
+                    Spacer()
+                }
+                .padding(.leading, 16)
             }
-            .padding(.leading, 16)
+            .navigationBarTitle("더나은냉장고")
+            .navigationBarItems(trailing:
+                                    Button(action: {
+                print("Done something")
+            }, label: {
+                Image(systemName: "plus")
+                    .resizable()
+                    .padding(6)
+                    .frame(width: 24, height: 24)
+                    .background(Color.blue)
+                    .clipShape(Circle())
+                    .foregroundColor(.white)
+            }))
         }
     }
 }
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
-        MainView(foods: [])
+        MainView(viewModel: MainViewModel())
     }
 }
