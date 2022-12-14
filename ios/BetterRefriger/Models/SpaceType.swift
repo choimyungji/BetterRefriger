5651e46f27d8449c4b7998c6f7a88587f0ea91b0
myungji <choimyungji@gmail.com>
2022-12-15 00:03:05 +0900

    fix import

--- ios/BetterRefriger/Models/SpaceType.swift
+++ ios/BetterRefriger/Models/SpaceType.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2019 maengji.com. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
 var baseSpaces: [SpaceType] = [
     SpaceType(keyString: "refriger", name: "냉장"),
--- ios/BetterRefriger/Service/FoodModelService.swift
+++ ios/BetterRefriger/Service/FoodModelService.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2018 maengji.com. All rights reserved.
 //
 
-import UIKit
+import Foundation
 import CoreData
 
 class FoodModelService: NSObject {
